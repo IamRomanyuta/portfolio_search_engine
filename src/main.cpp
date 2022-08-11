1b45b06b0ec66b38257654d59346f2f7d0de2b73
Andrey Romanyuta <97288234+IamRomanyuta@users.noreply.github.com>
2022-08-11 15:33:12 +0300

    delete useless comments

--- src/main.cpp
+++ src/main.cpp
@@ -7,25 +7,16 @@
 int main() {
 
   try {
-    // Creating a ConverterJSON object with a constructor with path parameters to JSON files
     ConverterJSON converter_json("../config.json", "../requests.json", "../answers.json");
 
-    // Creating a InvertedIndex obj
     InvertedIndex inverted_index;
 
-    /**
-     * ** Database update with
-     * Get.TextDocument() @return filepath from JSON config
-    */
+
     inverted_index.UpdateDocumentBase(converter_json.GetTextDocuments());
 
-    // Creating a SearchServer passing to constructor obj inverted_index
     SearchServer search_server(inverted_index);
 
-    /** @output in JSON response at the specified path
-     * ** Database update with
-     * Get.TextDocument() @return filepath from JSON config
-    */
+
     converter_json.putAnswers(
         search_server.search(
           converter_json.GetRequests(),
